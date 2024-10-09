f6dbe5a361f380be61e92fb66b78befacae98b20
Isuru Ariyarathna <isuruariyarathna2k00@gmail.com>
2024-10-09 17:07:31 +0530

    Initial Commit

---
+++ MathGame/MathGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MathGameApp.swift
+//  MathGame
+//
+//  Created by Isuru Ariyarathna on 2024-10-09.
+//
+
+import SwiftUI
+
+@main
+struct MathGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MathGameTests/MathGameTests.swift
@@ -0,0 +1,17 @@
+//
+//  MathGameTests.swift
+//  MathGameTests
+//
+//  Created by Isuru Ariyarathna on 2024-10-09.
+//
+
+import Testing
+@testable import MathGame
+
+struct MathGameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
