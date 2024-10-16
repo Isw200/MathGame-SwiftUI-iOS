03b4dfa3759b74c8c1db2b0450070f4741601a13
Isuru Ariyarathna <isuruariyarathna2k00@gmail.com>
2024-10-16 17:47:11 +0530

    change colours to emums

--- MathGame/Views/SettingsView.swift
+++ MathGame/Views/SettingsView.swift
@@ -7,10 +7,15 @@
 
 import SwiftUI
 
+enum AppTheme: String, CaseIterable {
+    case Red = "Red"
+    case Green = "Green"
+    case Blue = "Blue"
+}
+
 struct SettingsView: View {
-    var colors = ["Red", "Green", "Blue", "Tartan"]
     @AppStorage("fontSize") var fontSize: Double = 1
-    @AppStorage("selectedColor") var selectedColor: String = "Red"
+    @AppStorage("selectedColor") var selectedColor: AppTheme = .Red
     @StateObject var settingsViewModel: SettingsViewModel = SettingsViewModel()
     
     var body: some View {
@@ -19,24 +24,25 @@ struct SettingsView: View {
                 Text("Font Size: \(String(format: "%.1f", fontSize)) X")
                 Slider(value: $fontSize, in: 0.1...2)
                     .onChange(of: fontSize) { newValue in
-                        UserDefaults.standard.set(newValue, forKey: "fontSize")
+//                        UserDefaults.standard.set(newValue, forKey: "fontSize")
+                        fontSize = newValue
                     }
-                    .tint(settingsViewModel.getColor(for: selectedColor))
+//                    .tint(Color(selectedColor.rawValue))
                 
                 HStack {
                     Text("System Color")
                         .font(.system(size: 18 * fontSize))
                     Picker("Please choose a color", selection: $selectedColor) {
-                        ForEach(colors, id: \.self) {
-                            Text($0)
-                                .font(.system(size: 24 * fontSize))
+                        ForEach(AppTheme.allCases, id: \.self) { color in
+                            Text(color.rawValue)
+                                .font(.system(size: 18 * fontSize))
                         }
                     }
                     .pickerStyle(.wheel)
                     
                     Rectangle()
                         .frame(width: 40, height: 40)
-                        .foregroundColor(settingsViewModel.getColor(for: selectedColor))
+                        .foregroundColor(settingsViewModel.getColor(for: selectedColor.rawValue))
                         .cornerRadius(10)
                 }
                 
